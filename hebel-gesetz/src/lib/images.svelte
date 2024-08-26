e9609d8760d2d4f49f151df3a73117a6e563ca3d
Diego-Della-Vecchia <155764085+Diego-Della-Vecchia@users.noreply.github.com>
2024-08-26 05:52:07 +0000

    Add subtitles

--- hebel-gesetz/src/lib/images.svelte
+++ hebel-gesetz/src/lib/images.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
-  export let images: string[];
+  export let images: string[][];
 </script>
 
 <div class="flex justify-center items-center overflow-x-auto p-5">
   <div class="flex gap-2 items-center">
     {#each images as image}
-      <img class="rounded-md border-2 border-black h-32" src={image} alt="" />
+    <div>
+      <img class="rounded-md border-2 border-black h-32" src={image[0]} alt="" />
+      <p class="font-poppins text-center text-xl text-orange-500">{image[1]}</p>
+    </div>
     {/each}
   </div>
 </div>
