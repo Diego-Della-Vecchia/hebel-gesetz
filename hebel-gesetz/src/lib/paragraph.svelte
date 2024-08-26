ccca7b3eaa0e5fce85a9bce244ed24529cf8bcb0
Diego-Della-Vecchia <155764085+Diego-Della-Vecchia@users.noreply.github.com>
2024-08-26 06:16:21 +0000

    Add ability to make parts of texts bold

--- hebel-gesetz/src/lib/paragraph.svelte
+++ hebel-gesetz/src/lib/paragraph.svelte
@@ -1,12 +1,23 @@
 <script lang="ts">
     export let title : string
-    export let text : string[]
+    export let text : {text: string, type : string}[][]
 </script>
 
 <div class="flex justify-center items-center flex-col">
     <h1 class="text-5xl text-orange-500 p-5 font-poppins font-bold  text-center">{title}</h1>
-    {#each text as text }
-        <p class="text-left font-poppins text-xl md:mx-40 p-5 md:w-2/3">{text}</p> 
+    {#each text as paragraph }
+    {#each paragraph as text}
+    <p class="p-5 md:mx-40 md:w-2/3">
+        {#if text.type == "formel"}
+    
+        <span class="text-left font-poppins text-xl font-bold ">{text.text}</span> 
+        {:else}
+        <span class="text-left font-poppins text-xl">{text.text}</span> 
 
+        {/if}
+
+    </p>
+    
+    {/each}
     {/each}
 </div>
