9bbfd32f45983db447602b1d048db53a9974a085
Diego-Della-Vecchia <155764085+Diego-Della-Vecchia@users.noreply.github.com>
2024-08-26 05:46:46 +0000

    Add space between text

--- hebel-gesetz/src/lib/paragraph.svelte
+++ hebel-gesetz/src/lib/paragraph.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
     export let title : string
-    export let text : string
+    export let text : string[]
 </script>
 
 <div class="flex justify-center items-center flex-col">
     <h1 class="text-5xl text-orange-500 p-5 font-poppins font-bold  text-center">{title}</h1>
-<p class="text-left font-poppins text-xl md:mx-40 p-5 md:w-2/3">{text}</p> 
+    {#each text as text }
+        <p class="text-left font-poppins text-xl md:mx-40 p-5 md:w-2/3">{text}</p> 
+
+    {/each}
 </div>
